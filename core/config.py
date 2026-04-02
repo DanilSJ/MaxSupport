90bebc8dbe74d3873d9f79d4e6857dcbbf8a46ae
DanilSJ <danilchagarnoy@mail.ru>
2026-04-02 21:14:42 +0300

    add config and sqlalchemy

---
+++ core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+import pathlib
+
+load_dotenv()
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+
+class Settings(BaseSettings):
+    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
+    db_echo: bool = False
+
+    TOKEN: str = os.getenv("TOKEN")
+
+settings = Settings()
