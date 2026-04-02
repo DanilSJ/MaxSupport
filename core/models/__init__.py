108816d2088a78bfcd3d32fa648dd97380217e78
DanilSJ <danilchagarnoy@mail.ru>
2026-04-03 00:08:43 +0300

    create model user

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,8 +1,10 @@
 __all__ = [
     "Base",
+    "User",
     "DatabaseHelper",
     "db_helper",
 ]
 
 from .base import Base
+from .user import User
 from .db_helper import DatabaseHelper, db_helper
---
+++ core/models/user.py
@@ -0,0 +1,7 @@
+from sqlalchemy import BigInteger
+from sqlalchemy.orm import Mapped, mapped_column
+from .base import Base
+
+
+class User(Base):
+    max_id: Mapped[int] = mapped_column(BigInteger, unique=True)
