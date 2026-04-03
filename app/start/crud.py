b27b4640a61055dfa2fe05c3c29efab37f5a0716
DanilSJ <danilchagarnoy@mail.ru>
2026-04-04 00:06:50 +0300

    del func get_all_chats_id, get_user_with_chat

--- app/start/crud.py
+++ app/start/crud.py
@@ -26,19 +26,6 @@ async def get_all_users(session: AsyncSession) -> List[User]:
     users = result.scalars().all()
     return list(users)
 
-async def get_all_chats_id(session: AsyncSession) -> List[int]:
-    stmt = select(Chat.chat_id)
-    result = await session.execute(stmt)
-    chat_ids = result.scalars().all()
-    return list(chat_ids)
-
-
-async def get_user_with_chat(session: AsyncSession, max_id: int) -> User:
-    stmt = select(User).where(User.max_id == max_id)
-    result = await session.execute(stmt)
-    user = result.scalar_one_or_none()
-
-    return user
 
 async def get_all_chat(session: AsyncSession) -> List[Chat]:
     stmt = select(Chat)
