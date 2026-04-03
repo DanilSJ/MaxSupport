598d960a7e767781c77a9672bfdb174923e789cc
DanilSJ <danilchagarnoy@mail.ru>
2026-04-03 23:46:28 +0300

    new field parent children

--- core/models/chat.py
+++ core/models/chat.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Optional
 
-from sqlalchemy import BigInteger, String
+from sqlalchemy import BigInteger, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base
 
@@ -9,4 +9,21 @@ class Chat(Base):
     chat_id: Mapped[int] = mapped_column(BigInteger, unique=True)
     name: Mapped[str] = mapped_column(String)
 
-    users: Mapped[List["User"]] = relationship(back_populates="chat")
+    parent_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey("chats.id"),
+        nullable=True
+    )
+
+    parent: Mapped[Optional["Chat"]] = relationship(
+        remote_side="Chat.id",
+        back_populates="children"
+    )
+
+    children: Mapped[List["Chat"]] = relationship(
+        back_populates="parent"
+    )
+
+    users: Mapped[List["User"]] = relationship(
+        "User",
+        back_populates="chat"
+    )
--- core/models/user.py
+++ core/models/user.py
@@ -7,9 +7,11 @@ from .base import Base
 
 class User(Base):
     max_id: Mapped[int] = mapped_column(BigInteger, unique=True)
-    admin: Mapped[bool] = mapped_column(Boolean, default=False, nullable=True)
+    admin: Mapped[bool] = mapped_column(Boolean, default=False)
 
-    chat_id: Mapped[Optional[int]] = mapped_column(ForeignKey("chats.id"), nullable=True)
-
+    chat_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey("chats.id"),
+        nullable=True
+    )
 
     chat: Mapped[Optional["Chat"]] = relationship(back_populates="users")
