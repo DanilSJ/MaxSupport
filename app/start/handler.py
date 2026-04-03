f8fafbe1e1bf743a9ff42890fd6fb1dcd48b34c1
DanilSJ <danilchagarnoy@mail.ru>
2026-04-03 23:46:54 +0300

    generate inlinebutton city

--- app/start/handler.py
+++ app/start/handler.py
@@ -5,11 +5,30 @@ from maxapi.exceptions import MaxApiError
 from maxapi.enums.parse_mode import ParseMode
 from core.config import settings
 from core.models import db_helper
-from .crud import create_user, get_user_with_chat, get_all_chats_id
+from .crud import create_user, get_user_with_chat, get_all_chats_id, get_child_chats, get_root_chats, get_chat_by_id
 from maxapi.context import MemoryContext
 from app.utils.rate_limit import rate_limit
+from maxapi.types import CallbackButton
+from maxapi.utils.inline_keyboard import InlineKeyboardBuilder
+from maxapi.types import MessageCallback
+
 
 router = Router()
+
+async def build_cities_keyboard(session):
+    chats = await get_root_chats(session)
+
+    builder = InlineKeyboardBuilder()
+
+    for chat in chats:
+        builder.row(
+            CallbackButton(
+                text=f"🔗 {chat.name}",
+                payload=f"city_{chat.chat_id}"
+            )
+        )
+
+    return builder.as_markup()
 
 @router.bot_started()
 async def bot_started(event: BotStarted):
@@ -31,17 +50,62 @@ async def bot_started(event: BotStarted):
 @router.message_created(Command('start'))
 async def start(event: MessageCreated):
     async for session in db_helper.scoped_session_dependency():
-        await create_user(session, event.from_user.user_id)
+        keyboard = await build_cities_keyboard(session)
+
+        await event.message.answer(
+            "Приветствую 🖐\n\nВыберите город, где нужно разместить рекламу:",
+            attachments=[keyboard]
+        )
         break
 
-    await event.message.answer("""Здравствуйте 🖐
+@router.message_callback()
+async def handle_city(callback: MessageCallback):
+    data = callback.callback.payload  # city_1
 
-Чтобы получить актуальные расценки на рекламу, ответьте пожалуйста на пару вопросов:
+    if not data.startswith("city_"):
+        return
+
+    chat_id = int(data.split("_")[1])
+
+    async for session in db_helper.scoped_session_dependency():
+        print(chat_id)
+        chat = await get_chat_by_id(session, chat_id)
+        if not chat:
+            await callback.message.answer("Ошибка: выбранный город не найден в базе.")
+            return
+
+        children = await get_child_chats(session, chat.id)
+
+        if children:
+            builder = InlineKeyboardBuilder()
+
+            for child in children:
+                builder.row(
+                    CallbackButton(
+                        text=f"🔗 {child.name}",
+                        payload=f"city_{child.chat_id}"
+                    )
+                )
+
+            await callback.message.answer(
+                f"Выберите район ({chat.name}):",
+                attachments=[builder.as_markup()]
+            )
+            return
+
+        await create_user(
+            session,
+            callback.from_user.user_id,
+            chat_id=chat.chat_id
+        )
+
+        await callback.message.answer(
+            """Чтобы получить актуальные расценки на рекламу, ответьте пожалуйста на пару вопросов:
 1️⃣ Что планируете рекламировать?
-2️⃣ Где находится Ваше заведение (либо по какому адресу предоставляете услугу)?
+2️⃣ Где находится Ваше заведение (либо по какому адресу предоставляете услугу)?"""
+        )
 
-Мы ответим Вам в ближайшее время 😉""")
-
+        break
 
 @router.message_created(F.message.body)
 @rate_limit(limit=2, seconds=2)
