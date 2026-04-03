4ea1d281cf5dba8b42eb8a094396727f3551304d
DanilSJ <danilchagarnoy@mail.ru>
2026-04-03 23:02:06 +0300

    new func get_all_chats_id get_user_with_chat

--- app/start/crud.py
+++ app/start/crud.py
@@ -1,7 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from core.models import User
-from typing import List
+from sqlalchemy.orm import selectinload
+
+from core.models import User, Chat
+from typing import List, Optional
 
 
 async def create_user(session: AsyncSession, max_id: int) -> User:
@@ -22,3 +24,17 @@ async def get_all_users(session: AsyncSession) -> List[User]:
     result = await session.execute(stmt)
     users = result.scalars().all()
     return list(users)
+
+async def get_all_chats_id(session: AsyncSession) -> List[int]:
+    stmt = select(Chat.chat_id)
+    result = await session.execute(stmt)
+    chat_ids = result.scalars().all()
+    return list(chat_ids)
+
+
+async def get_user_with_chat(session: AsyncSession, max_id: int) -> User:
+    stmt = select(User).where(User.max_id == max_id)
+    result = await session.execute(stmt)
+    user = result.scalar_one_or_none()
+
+    return user
