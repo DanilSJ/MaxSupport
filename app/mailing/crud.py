b1ba3c1c8ba35ea8b5b09385b2f874a75928664b
DanilSJ <danilchagarnoy@mail.ru>
2026-04-04 00:02:00 +0300

    create get_user

--- app/mailing/crud.py
+++ app/mailing/crud.py
@@ -9,3 +9,8 @@ async def get_all_users(session: AsyncSession) -> List[User]:
     result = await session.execute(stmt)
     users = result.scalars().all()
     return list(users)
+
+async def get_user(session: AsyncSession, max_id: int) -> User | None:
+    stmt = select(User).where(User.max_id == max_id)
+    result = await session.execute(stmt)
+    return result.scalar_one_or_none()
