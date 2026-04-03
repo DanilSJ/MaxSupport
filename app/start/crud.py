d579de99d04a131343aa2ed59213981e4ee597a1
DanilSJ <danilchagarnoy@mail.ru>
2026-04-03 23:46:12 +0300

    create get_all_chat get_chat_by_id get_child_chats get_root_chats

--- app/start/crud.py
+++ app/start/crud.py
@@ -1,21 +1,22 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from sqlalchemy.orm import selectinload
-
 from core.models import User, Chat
 from typing import List, Optional
 
 
-async def create_user(session: AsyncSession, max_id: int) -> User:
+async def create_user(session: AsyncSession, max_id: int, chat_id: int | None = None) -> User:
     stmt = select(User).where(User.max_id == max_id)
     result = await session.execute(stmt)
     user = result.scalar_one_or_none()
 
     if not user:
-        user = User(max_id=max_id)
+        user = User(max_id=max_id, chat_id=chat_id)
         session.add(user)
-        await session.commit()
+    else:
+        if chat_id is not None:
+            user.chat_id = chat_id  # обновляем
 
+    await session.commit()
     return user
 
 
@@ -38,3 +39,25 @@ async def get_user_with_chat(session: AsyncSession, max_id: int) -> User:
     user = result.scalar_one_or_none()
 
     return user
+
+async def get_all_chat(session: AsyncSession) -> List[Chat]:
+    stmt = select(Chat)
+    result = await session.execute(stmt)
+    chats = result.scalars().all()
+    return list(chats)
+
+async def get_chat_by_id(session: AsyncSession, chat_id: int) -> Optional[Chat]:
+    stmt = select(Chat).where(Chat.chat_id == chat_id)
+    result = await session.execute(stmt)
+    return result.scalar_one_or_none()
+
+
+async def get_child_chats(session: AsyncSession, parent_id: int):
+    stmt = select(Chat).where(Chat.parent_id == parent_id)
+    result = await session.execute(stmt)
+    return result.scalars().all()
+
+async def get_root_chats(session: AsyncSession) -> List[Chat]:
+    stmt = select(Chat).where(Chat.parent_id == None)
+    result = await session.execute(stmt)
+    return list(result.scalars().all())
