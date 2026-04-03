bff027143ab305e239ef7e377764f190fe1b14ab
DanilSJ <danilchagarnoy@mail.ru>
2026-04-04 00:07:11 +0300

    del func echo and del not use import

--- app/start/handler.py
+++ app/start/handler.py
@@ -1,13 +1,8 @@
-from maxapi import Router, F
+from maxapi import Router
 from maxapi.types import MessageCreated, Command, BotStarted
 from core.config import bot
-from maxapi.exceptions import MaxApiError
-from maxapi.enums.parse_mode import ParseMode
-from core.config import settings
 from core.models import db_helper
-from .crud import create_user, get_user_with_chat, get_all_chats_id, get_child_chats, get_root_chats, get_chat_by_id
-from maxapi.context import MemoryContext
-from app.utils.rate_limit import rate_limit
+from .crud import create_user, get_child_chats, get_root_chats, get_chat_by_id
 from maxapi.types import CallbackButton
 from maxapi.utils.inline_keyboard import InlineKeyboardBuilder
 from maxapi.types import MessageCallback
@@ -49,14 +44,15 @@ async def bot_started(event: BotStarted):
 
 @router.message_created(Command('start'))
 async def start(event: MessageCreated):
+    keyboard = []
     async for session in db_helper.scoped_session_dependency():
         keyboard = await build_cities_keyboard(session)
-
-        await event.message.answer(
-            "Приветствую 🖐\n\nВыберите город, где нужно разместить рекламу:",
-            attachments=[keyboard]
-        )
         break
+
+    await event.message.answer(
+        "Приветствую 🖐\n\nВыберите город, где нужно разместить рекламу:",
+        attachments=[keyboard]
+    )
 
 @router.message_callback()
 async def handle_city(callback: MessageCallback):
@@ -106,38 +102,3 @@ async def handle_city(callback: MessageCallback):
         )
 
         break
-
-@router.message_created(F.message.body)
-@rate_limit(limit=2, seconds=2)
-async def echo(event: MessageCreated, context: MemoryContext):
-
-    current_state = await context.get_state()
-
-    if current_state is not None:
-        return
-
-    try:
-
-        async for session in db_helper.scoped_session_dependency():
-            user = await get_user_with_chat(session, event.from_user.user_id)
-
-            chats_id = await get_all_chats_id(session)
-
-            if event.chat.chat_id not in chats_id:
-                return await event.message.forward(chat_id=user.chat_id)
-
-
-            if event.message.link.sender.user_id == 230120179:
-                user = await bot.get_message(event.message.link.message.mid)
-
-                try:
-                    return await bot.send_message(user_id=user.link.sender.user_id, text=event.message.body.text)
-                except MaxApiError:
-                    return await bot.send_message(chat_id=settings.GROUP,
-                                                  text=f"<a href='max://user/{user.link.sender.user_id}'>Пользователь</a> заблокировал бота",
-                                                  parse_mode=ParseMode.HTML)
-
-            break
-    except Exception as e:
-        print(e)
-        return await event.message.answer("Бот не добавлен в группу")
