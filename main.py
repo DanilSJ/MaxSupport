f7c2198f19bf91177316fff002b40974a44c77ec
DanilSJ <danilchagarnoy@mail.ru>
2026-04-04 00:07:38 +0300

    include mailing_router echo_router

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from maxapi import Dispatcher
 from core.config import bot
 from app.start.handler import router as start_router
 from app.mailing.handler import router as mailing_router
+from app.echo.handler import router as echo_router
 
 logging.basicConfig(level=logging.INFO)
 dp = Dispatcher()
 
 async def main():
-    dp.include_routers(start_router)
     dp.include_routers(mailing_router)
+    dp.include_routers(start_router)
+    dp.include_routers(echo_router)
 
     await asyncio.gather(
         dp.start_polling(bot)
