ef13a70ed995066623162ebff2060df8f017373c
DanilSJ <danilchagarnoy@mail.ru>
2026-04-20 23:35:56 +0300

    create get_message_by_admin_id and add new field in create_message (admin_message_id, user_message_id)

--- app/utils/crud.py
+++ app/utils/crud.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import Message
 
@@ -6,12 +7,16 @@ async def create_message(
         session: AsyncSession,
         max_id: int,
         chat_id: int,
+        admin_message_id: str | None,
+        user_message_id: str,
         answer: bool = False,
         question: bool = False,
 ) -> Message:
     message = Message(
         max_id=max_id,
         chat_id=chat_id,
+        admin_message_id=admin_message_id,
+        user_message_id=user_message_id,
         answer=answer,
         question=question
     )
@@ -21,3 +26,12 @@ async def create_message(
     await session.refresh(message)
 
     return message
+
+async def get_message_by_admin_id(
+        session: AsyncSession,
+        admin_message_id: str
+) -> Message | None:
+    result = await session.execute(
+        select(Message).where(Message.admin_message_id == admin_message_id)
+    )
+    return result.scalar_one_or_none()
